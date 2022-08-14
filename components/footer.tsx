b451c7e231c1e036d876ac64583c456b6d9f2f17
Aerea One <aereaone@proton.me>
2022-08-15 00:23:33 +0200

    update link:

--- components/footer.tsx
+++ components/footer.tsx
@@ -19,15 +19,9 @@ const Footer = () => {
             Connect with me.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            {/* <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Email me
-            </a> */}
             <BasicModal/>
             <a
-              href={`instagram.com/aerea.lab`}
+              href="https://instagram.com/aerea.lab"
               className="mx-3 font-bold hover:underline"
             >
               Instagram
